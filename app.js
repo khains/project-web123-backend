c8e7059d37aab6afc7543fb9fc9622219f6757cf
khains <nskhai3897@gmail.com>
2019-11-07 19:24:49 +0700

    up1

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const apiRouter = require('./routers/api');
+
+const app = express();
+
+app.use(cors({ origin: ['http://localhost:3000'] , credentials : true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://khains:sykhai123@ds243148.mlab.com:43148/project-123', (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(" DB connect success!");
+    }
+})
+
+app.use('/api', apiRouter);
+
+app.listen(process.env.PORT || 6969, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Server start success!!")
+    }
+})
---
+++ model/merchandises.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const MerchandiseSchema = new Schema({
+    id :{
+        type : String,
+        required : true,
+        unique : true,
+    },
+    image : {
+        type : String,
+        required : true
+    },
+    name :{
+        type : String,
+        required : true
+    },
+    size : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    price :{
+        type : Number,
+        required : true
+    },
+    types :{
+        type : String,
+        required : true 
+    }
+});
+
+module.exports = mongoose.model('Merchandise' ,MerchandiseSchema)
